aab1785efd1dbe9550d78fc6e921ba12c8a26718
Joshua Charley <charley1629099@orange-sonatel.com>
2021-12-12 15:19:06 +0000

    wipserver

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passportConfig = require('./lib/passportConfig');
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./lib/passportConfig");
+const cors = require("cors");
+const fs = require("fs");
 
 // MongoDB
 mongoose
-  .connect(
-    'mongodb+srv://josh-user:joshuamongodb@cluster0.dtbu2.mongodb.net/businessreg?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then((res) => console.log('Connected to DB'))
+  .connect("mongodb://localhost:27017/jobPortal", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
 // initialising directories
-if (!fs.existsSync('./public')) {
-  fs.mkdirSync('./public');
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
 }
-if (!fs.existsSync('./public/resume')) {
-  fs.mkdirSync('./public/resume');
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
 }
-if (!fs.existsSync('./public/profile')) {
-  fs.mkdirSync('./public/profile');
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
 }
 
 const app = express();
@@ -42,10 +39,10 @@ app.use(express.json());
 app.use(passportConfig.initialize());
 
 // Routing
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/api', require('./routes/apiRoutes'));
-app.use('/upload', require('./routes/uploadRoutes'));
-app.use('/host', require('./routes/downloadRoutes'));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+app.use("/host", require("./routes/downloadRoutes"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
